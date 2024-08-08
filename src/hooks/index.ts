f5e3a1e2114bed0dbe286047cf54209d594e6f08
kaiwen <617297811@qq.com>
2024-08-08 21:45:11 +0800

    chore: useClickAway, useTimeoutFn, useDebounce

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,6 @@ export { default as useAsyncFn } from './useAsyncFn'
 export { default as useMountedState } from './useMountedState'
 export { default as useEffectOnce } from './useEffectOnce'
 export { default as useMount } from './useMount'
+export { default as useClickAway } from './useClickAway'
+export { default as useTimeoutFn } from './useTimeoutFn';
+export { default as useDebounce } from './useDebounce';
---
+++ src/hooks/useTimeoutFn.ts
@@ -0,0 +1,38 @@
+import {useCallback, useEffect, useRef} from "react";
+
+export type UseTimeoutFnReturn = [() => boolean | null, () => void, () => void]
+
+export default function useTimeoutFn(fn: Function, ms: number = 0): UseTimeoutFnReturn {
+  const ready = useRef<boolean | null>(false)
+  const timeout = useRef<ReturnType<typeof setTimeout>>()
+  const callback = useRef(fn)
+
+  const isReady = useCallback(() => ready.current, [])
+
+  const set = useCallback(() => {
+    ready.current = false;
+    timeout.current && clearTimeout(timeout.current)
+
+    timeout.current = setTimeout(() => {
+      ready.current = true;
+      callback.current()
+    }, ms)
+  }, [ms])
+
+  const clear = useCallback(() => {
+    ready.current = null
+    timeout.current && clearTimeout(timeout.current)
+  }, [])
+
+  useEffect(() => {
+    callback.current = fn
+  }, [fn])
+
+  useEffect(() => {
+    set()
+
+    return clear
+  }, [ms])
+
+  return [isReady, clear, set]
+}
